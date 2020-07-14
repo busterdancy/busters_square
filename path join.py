1ba04f4d05545f502f2274cf115632e0515116b4
busterdancy <66982955+busterdancy@users.noreply.github.com>
2020-07-14 03:29:05 -0500

    what is this
    Was that just a description of commit changes things and this is just extension?

---
+++ path join.py
@@ -0,0 +1,10 @@
+import os
+'''
+first = '/home/busterdancy/Documents/Programming'
+second = 'T:158, K:40'
+
+
+last = os.path.join(first, 'Graphs: '+second+'.jpg')
+print(last)
+'''
+print(os.path.join('this','is','a','path'))
